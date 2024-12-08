24c2350c2fcf8b3dc8e37cc473267368abf9a00e
Abdallah-ELfaramawy <abdalallahwalid@gmail.com>
2024-12-08 19:02:08 +0200

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,13 @@
+package project;
+
+public abstract class Parent {
+    public int id;
+    public String name;
+    public String contactInfo;
+
+    public abstract void viewViolations();
+
+
+
+
+}
